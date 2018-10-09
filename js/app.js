d78b7f48d5c38929ff8ff550c26bcdf9766a348a
rkgallaway <rkgallaway@msn.com>
2018-10-09 15:29:46 -0700

    Questions 1 thru 4 working

--- js/app.js
+++ js/app.js
@@ -6,17 +6,49 @@ var username = prompt('What is your name?');
 
 alert('Welcome, ' + username + '! Let\'s get started');
 
-var answerOne = prompt('Do I have more than three pets?').toLowerCase();
+var answerOne = prompt('Do I have a pet?').toLowerCase();
 
 if (answerOne === 'yes' || answerOne === 'y') {
     //tell them
-    alert('You got it right! I have a dog and four cats for a total of five beasts!');
+    alert('You got it right! I have a dog; her name is Weiner: The Dog!');
     //console.log the result
     console.log('user got question 1 correct');
 } else {
     //tell them
-    alert('you fail, I have five pets')
+    alert('you FAIL, I have a dog; her name is Weiner : The Dog')
     //consult.log the result
     console.log('user got question 1 wrong');
-
 }
+
+var answerTwo = prompt('Do I live in Seattle?').toLowerCase();
+
+if (answerTwo === 'yes' || answerTwo === 'y') {
+    //tell them
+    alert('You got it right! I live in Seattle in the Beacon Hill neighborhood!');
+    //console.log the result
+    console.log('user got question 1 correct');
+} else {
+    //tell them
+    alert('you FAIL, I live in Seattle in the Beacon Hill neighborhood!')
+    //consult.log the result
+    console.log('user got question 2 wrong');
+}
+
+var answerThree = prompt('Do I like Baseball').toLowerCase();
+
+if (answerThree === 'yes' || answerThree === 'y'){
+    alert('Yes I do!  I\'m not super baseball-wise yet, but I love going to see a Mariners game -food and drinks!');
+    console.log('user got question 3 wrong')
+} else {
+    alert('you Fail,  I\'m not super baseball-wise yet, but I love going to see a Mariners game -food and drinks!');
+}
+
+var answerFour = prompt('Do I like Foodball').toLowerCase();
+
+if (answerFour === 'no' || answerFour === 'n'){
+    alert('That\'s correct!  I dont\'t love the football... even with food and drinks!');
+    console.log('user got question 3 wrong')
+} else {
+    alert('you Fail,  I don\'t love the football, even with food and drink!');
+}
+
